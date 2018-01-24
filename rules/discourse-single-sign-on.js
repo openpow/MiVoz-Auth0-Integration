165d1f00b9af44d7e1c3af078900643009fdd83e
Leo Giovanetti <hello@leog.me>
2018-01-24 01:06:37 -0300

    Cambiando a que cliente aplica el SSO dependiendo de una variable de entorno

--- rules/discourse-single-sign-on.js
+++ rules/discourse-single-sign-on.js
@@ -1,5 +1,5 @@
 function (user, context, callback) {
-    if(context.clientID === "UV33huHnJUSqyKCBuyBu5s9CSbTAdOES") {
+    if(context.clientID === configuration.SSO_CLIENT_ID) {
         var discourse_sso = require('discourse-sso');
         var sso = new discourse_sso(context.clientMetadata.sso_secret);
 
@@ -12,7 +12,7 @@ function (user, context, callback) {
                 "external_id": user.user_id,
                 "email": user.email,
                 // Optional
-                "username": user.user_metadata.full_name.replace(" ", ""),
+                "username": user.user_metadata.nickname,
                 "name": user.user_metadata.full_name,
                 "require_activation": !user.email_verified,
                 "suppress_welcome_message": true
@@ -21,7 +21,7 @@ function (user, context, callback) {
             var q = sso.buildLoginString(userparams);
 
             context.redirect = {
-                url: "http://localhost:4000/session/sso_login?" + q
+                url: "https://debate.partidodigital.org.uy/session/sso_login?" + q
             };
         }
     }
