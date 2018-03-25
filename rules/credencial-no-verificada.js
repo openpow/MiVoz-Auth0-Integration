f4b46e9ae861d9bf5fa0e747c6b2169cf34b7666
Leo Giovanetti <hello@leog.me>
2018-03-24 22:30:06 -0300

    Cambios de configuración

--- rules/credencial-no-verificada.js
+++ rules/credencial-no-verificada.js
@@ -1,11 +1,6 @@
 function(user, context, callback){
     user.app_metadata = user.app_metadata || {};
     user.app_metadata.credencial_verificada = false;
-    user.user_metadata = user.user_metadata || {};
-    user.user_metadata.credencial_verificada = null;
-
-    // clean up user_metadata
-    auth0.users.updateUserMetadata(user.user_id, user.user_metadata);
 
     // persist the app_metadata update
     auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
--- rules/discourse-chequeo-correo-verificado.js
+++ rules/discourse-chequeo-correo-verificado.js
@@ -1,6 +1,6 @@
 function (user, context, callback) {
-    if (!user.email_verified && context.clientID === '3yqAXfyeQCu5RcrgSQUsb0M4ojdvif4N') {
-        return callback(new UnauthorizedError('Por favor verifica tu dirección de correo para validar tu cuenta y poder ingresar a la herramienta de Debate del Partido Digital'));
+    if (!user.email_verified) {
+        return callback(new UnauthorizedError('Por favor verifica tu dirección de correo para validar tu cuenta y poder entrar a MiVoz'));
     }
     return callback(null, user, context);
 }
